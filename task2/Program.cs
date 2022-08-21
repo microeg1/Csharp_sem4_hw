95e9c4d0f470b50fea931e6276c68685bea3268e
Egor <pro100egar@gmail.com>
2022-08-21 16:51:39 +0700

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int s = 0;
+            while (a > 0)
+            {
+                s = s + a % 10;
+                a = a /10 ;
+            }
+        Console.WriteLine(s);
+        
+
+
+    }
+}
