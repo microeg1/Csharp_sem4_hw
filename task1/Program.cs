415734a82098c4158d3fe53a90f174fb473f12d8
Egor <pro100egar@gmail.com>
2022-08-21 16:51:30 +0700

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите введите степень:");
+        int deg = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Число " + number + " в " + deg + " степени, равняется " + Math.Pow(number, deg) + ".");
+    }
+}
