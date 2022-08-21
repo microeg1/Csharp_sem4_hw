583285eb65c303dd5e7f0b3dea3c30905045bbaa
Egor <pro100egar@gmail.com>
2022-08-21 16:51:47 +0700

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+class Program
+{
+    static void PrintArray(int[] col)             // печатает список
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write(col[position] + ", ");
+        position++;
+    }
+}
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите желаемое количество элементов в массиве:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[n];
+        int index = 0;
+        while (index < n)
+        {
+            numbers[index] = new Random().Next();
+            index++;
+            
+        }
+    PrintArray(numbers);
+        
+        
+
+
+    }
+}
